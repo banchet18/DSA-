7753a2b9512afc3cc22b4c5ebc89309a73a5b805
banchet18 <banjarac6@gmail.com>
2024-01-14 08:24:47 +0530

    c

--- basicquestions/2.findminimumofthree.js
+++ basicquestions/2.findminimumofthree.js
@@ -1,15 +1,30 @@
+// approach 1
+
+// function minimum(x, y, z) {
+//   if (x < y) {
+//     if (x < z) {
+//       console.log(x);
+//     } else {
+//       console.log(z);
+//     }
+//   } else if (y < z) {
+//     console.log(y);
+//   } else {
+//     console.log(z);
+//   }
+// }
+
+// minimum(10, 100, 5);
+
+// approach 2
+
 function minimum(x, y, z) {
-  if (x < y) {
-    if (x < z) {
-      console.log(x);
-    } else {
-      console.log(z);
-    }
+  if (x < y && x < z) {
+    console.log(x);
   } else if (y < z) {
     console.log(y);
   } else {
     console.log(z);
   }
 }
-
-minimum(10, 100, 5);
+minimum(10, 5, 30);
