566e5468f7f92213fb2f60564bbcf75875905fd6
banchet18 <banjarac6@gmail.com>
2024-01-15 09:23:28 +0530

    c

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-const checkTriangle = () => {
+const checkTriangle = (a, b, c) => {
   if (a === b && b === c && c === a) {
     console.log("eqvilateral");
   } else if (a !== b && b !== c && c !== a) {
@@ -7,3 +7,5 @@ const checkTriangle = () => {
     console.log("isoscaline");
   }
 };
+
+checkTriangle(7, 6, 8);
